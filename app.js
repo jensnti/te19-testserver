b212f97e686602f52e492a5fef270dc5422bca5a
Jens Andreasson <jens.andreasson@ntig.se>
2022-01-21 11:25:01 +0100

    ✨ tasks router .env

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
 
+require('dotenv').config();
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const tasksRouter = require('./routes/tasks');
 
 const app = express();
 
@@ -23,5 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tasks', tasksRouter);
 
 module.exports = app;
