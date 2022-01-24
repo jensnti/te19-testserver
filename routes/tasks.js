659316dc8ebce72ce33a8e1f25fdd8f89c32547d
Jens Andreasson <jens.andreasson@ntig.se>
2022-01-24 11:09:20 +0100

    ✨ måndag v4 - json.res error :id

--- routes/tasks.js
+++ routes/tasks.js
@@ -13,9 +13,39 @@ router.get('/', async (req, res, next) => {
     await pool.promise()
         .query('SELECT * FROM tasks')
         .then(([rows, fields]) => {
-            console.log(rows);
-            res.json(rows);
+            res.json({
+                tasks: {
+                    data: rows
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                tasks: {
+                    error: 'Error getting tasks'
+                }
+            })
         });
+});
+
+router.get('/:id', async (req, res, next) => {
+    const id = req.params.id;
+
+    if (isNaN(req.params.id)) {
+        res.status(400).json({
+            task: {
+                error: 'Bad request'
+            }
+        });
+    }
+
+    // hämta raden från databasen med 
+    // SELECT * FROM tasks WHERE id = id
+    res.json({
+        id: req.params.id
+    })
+
 });
 
 module.exports = router;
