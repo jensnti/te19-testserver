cc5e008e80fa645c7f356e9062bd4a6cf7eca574
Jens Andreasson <jens.andreasson@ntig.se>
2022-02-28 09:41:17 +0100

    💄 fix index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+    res.render('index.njk', {
+        title: 'Te 19',
+        layout: 'layout.njk',
+    });
 });
 
 module.exports = router;
