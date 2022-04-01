476b9935cafbbaa2f51cb1c923ce42971a679cf5
Jens Andreasson <jensandreasson77@gmail.com>
2022-04-01 13:42:48 +0200

    Revert "🚧 heroku hosting"
    This reverts commit b0fbaf9ff0d67846fd03a1dc84a50f71ca8ad919.

--- database.js
+++ database.js
@@ -1,26 +1,12 @@
 const mysql = require('mysql2');
-const Pool = require('pg-pool');
 
-let pool;
-console.log(process.env.HEROKU)
-if (process.env.HEROKU === true) {
-    pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false,
-        },
-    });
-} else {
-    pool = mysql.createPool({
-        connectionLimit: 10,
-        charset: 'utf8mb4',
-        host: process.env.DATABASE_HOST,
-        user: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_DATABASE,
-    });
-}
-
-console.log(pool)
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    charset: 'utf8mb4',
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DATABASE,
+});
 
 module.exports = pool;
