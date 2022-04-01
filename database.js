b0fbaf9ff0d67846fd03a1dc84a50f71ca8ad919
Jens Andreasson <jensandreasson77@gmail.com>
2022-04-01 13:00:29 +0200

    🚧 heroku hosting

--- database.js
+++ database.js
@@ -1,12 +1,26 @@
 const mysql = require('mysql2');
+const Pool = require('pg-pool');
 
-const pool = mysql.createPool({
-    connectionLimit: 10,
-    charset: 'utf8mb4',
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_DATABASE,
-});
+let pool;
+console.log(process.env.HEROKU)
+if (process.env.HEROKU === true) {
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    });
+} else {
+    pool = mysql.createPool({
+        connectionLimit: 10,
+        charset: 'utf8mb4',
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_DATABASE,
+    });
+}
+
+console.log(pool)
 
 module.exports = pool;
